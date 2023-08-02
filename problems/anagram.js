b57866f34195ad58812ef650221fae1a4f20eb71
adil-khursheed <adilkhursheed60@gmail.com>
2023-08-02 18:30:43 +0530

    anagram

---
+++ problems/anagram.js
@@ -0,0 +1,46 @@
+// Q3: Valid Anagram
+/*
+An anagram is a word or phrase formed by rearranging the letters of a different word or phrase,
+using all the original letters exactly once.
+*/
+
+/*
+Example 1:
+Input: s = "anagram", t = "nagaram"
+Output: true
+
+Example 2:
+Input: s = "rat", t = "car"
+Output: false
+*/
+
+// 1st Solution
+// const isAnagram = function (s, t) {
+//   s = s.split("").sort().join("");
+//   t = t.split("").sort().join("");
+
+//   return s === t;
+// };
+
+// isAnagram("rat", "car");
+
+// 2nd Solution
+const isAnagram = function (s, t) {
+  if (s.length !== t.length) return false;
+
+  let obj1 = {};
+  let obj2 = {};
+
+  for (let i = 0; i < s.length; i++) {
+    obj1[s[i]] = (obj1[s[i]] || 0) + 1;
+    obj2[t[i]] = (obj2[t[i]] || 0) + 1;
+  }
+
+  for (const key in obj1) {
+    if (obj1[key] !== obj2[key]) return false;
+  }
+
+  return true;
+};
+
+isAnagram("anagram", "nagaram");
